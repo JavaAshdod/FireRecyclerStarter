dbad299e255864f4e416ebc8476eeef1c79ee66c
teach104 <teach104>
2016-07-21 11:03:40 +0300

    initial commit

---
+++ FireDatabase-master/app/src/main/java/tomer/edu/firedemo/models/Message.java
@@ -0,0 +1,44 @@
+package tomer.edu.firedemo.models;
+
+/**
+ * Created by stud27 on 20/07/16.
+ */
+public class Message {
+    private String title;
+    private String content;
+
+    public Message() {
+    }
+
+
+
+    public Message(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
